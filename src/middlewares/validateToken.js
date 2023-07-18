8cd9ea5cb269d8458f0dec5f4ca3a2f34bbf1b78
Amanda Aragão <amandaaragao72@gmail.com>
2023-07-17 22:08:36 -0300

    fix: req3

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -7,7 +7,7 @@ const validateToken = (req, res, next) => {
       return res.status(401).json({ message: 'Token not found' });
     }
     const token = authorization.includes('Bearer') ? authorization.split(' ')[1] : authorization;
-   
+
     const payloadToken = getPayload(token);
     req.payload = payloadToken;
     next();
